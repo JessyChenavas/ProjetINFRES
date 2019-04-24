012e4239d6c8ad0e07cf375b6402bbe4a70cb085
guillaumelagay <guillaume.lagay1@gmail.com>
2019-04-24 09:52:04 +0200

    - OAuth config

--- src/Controller/Api/ApiSecurityController.php
+++ src/Controller/Api/ApiSecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,9 +26,9 @@ class ApiSecurityController extends AbstractController
     public function AuthenticationAction(Request $request)
     {
         $data = json_decode($request->getContent(), true);
-//        if (empty($data['redirect-uri']) || empty($data['grant-type'])) {
-//            return $this->handleView($this->view($data));
-//        }
+        if (empty($data['redirect-uri']) || empty($data['grant-type'])) {
+            return new JsonResponse($data);
+        }
         $clientManager = $this->client_manager;
         $client = $clientManager->createClient();
         $client->setRedirectUris([$data['redirect-uri']]);
@@ -36,6 +37,7 @@ class ApiSecurityController extends AbstractController
         $rows = [
             'client_id' => $client->getPublicId(), 'client_secret' => $client->getSecret()
         ];
-        return new Response('Ok');
+
+        return new JsonResponse($rows);
     }
 }
